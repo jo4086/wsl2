0bc62ef2fb8b61b3c88d5035cd5b2798b8db8b75
jo4086 <ghzktm5892@gmail.com>
2025-05-13 23:08:44 +0900

    [etc]: 기타 등등 - index.html: icon link 추가 - vite.config.ts: scss 별칭 추가 - tsconfig.app.json: styles 별칭 추가 - package.json:   - dependencies: classnames   - devDependeencies: @types/node, sass - App.tsx: 스크롤 관련 로직 이동, 최상위를 Scroll, Wrapper, Navbar로   감싸서 기본 구조 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 // import { Navigate } from './components/shared/Navigate'
+import Home from './domain/home/Home'
+// import Homie
 import './global.css'
+import { Scrollbar } from './components/shared/Scrollbar'
 import { Navbar } from './components/Navi'
+import { Wrapper } from './components/shared/Wrapper'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY
+    const stopTop = 70
+    let lastTop = 1000
+
+    const handleScroll = () => {
+      const currentY = window.scrollY
+      const isScrollingDown = currentY > lastScrollY
+
+      if (isScrollingDown && currentY > 0) {
+        setIsScrolled(true)
+      } else if (!isScrollingDown && currentY < 30) {
+        setIsScrolled(false)
+      }
+
+      lastScrollY = currentY
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
-    <div style={{ height: '1000px' }}>
-      <Navbar />
+    <div style={{ height: '3000px' }}>
+      <Scrollbar maxHeight="10px">
+        <Navbar isScrolled={isScrolled} />
+        <Wrapper isScrolled={isScrolled}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Wrapper>
+      </Scrollbar>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,22 @@ export default defineConfig({
   server: {
     host: true, // ← 핵심 설정
     port: 5173, // 필요 시 고정 포트 지정
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@scss': path.resolve(__dirname, './src/styles/scss'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // api: 'legacy',
+        // includePaths: ['src'],
+        additionalData: `@use "@styles/scss/forward.scss" as style;`,
+      },
+    },
   },
 })
