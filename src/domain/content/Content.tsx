1abdb784398e308c5e9e3e14366c637b9f5dd481
jo4086 <ghzktm5892@gmail.com>
2025-05-14 21:43:50 +0900

    [Feat]: 테스트용 도메인 추가 (Content)
    - desc: 도메인의 자식 폴더명 = 최상위 도메인 컴포넌트 구성
    - Home(.tsx, index.ts, ...) `link="/"`
    - Content(.tsx, index.ts, ...) `link="/content`

---
+++ src/domain/content/Content.tsx
@@ -0,0 +1,5 @@
+const Content = () => {
+  return <div>Content Page</div>
+}
+
+export default Content
--- src/domain/home/Home.tsx
+++ src/domain/home/Home.tsx
@@ -1,14 +1,11 @@
 import { Main } from './Main'
-import '../../styles/App.css'
-import '../../components/shared/Wrapper.scss'
+// import '../../styles/App.css'
+// import '../../components/shared/Wrapper.scss'
 import cn from 'classnames'
+import { Wrapper } from '@/components/wrapper'
 
-const Home = () => {
-  return (
-    <Main>
-      <div>hi</div>
-    </Main>
-  )
+const Home = ({}) => {
+  return <div>hiasdasdsasadsa</div>
 }
 
 export default Home
